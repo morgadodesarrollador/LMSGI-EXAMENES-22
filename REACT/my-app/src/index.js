99ad43280c6c5bb5a3bb5ef5b5b5ad07a767fef7
Trino10 <trinotijola10@gmail.com>
2023-01-10 12:31:50 +0100

    react

---
+++ REACT/my-app/src/index.js
@@ -0,0 +1,37 @@
+console.log(`Hola Mundo...`);
+console.log(`tenemos un problema`);
+
+
+//*****************JAVASCRIPT*********************
+
+//CONSTANTES
+// IDENTIFICADOR --> dirección de memoria RAM
+const nombre = "trino";
+const edad = 25; // tipo(25) = Number
+
+//{const | let} identificador = valor;
+//valor --> tipo
+
+//EC6 --> Emacs Script 6
+
+//VARIABLES
+let color = "rojo";//tipo ("rojo") --> String (cadena de caracteres)
+console.log ("El color es", color);
+
+//console.log (nombre,edad,color);
+
+//color = "verde";
+
+//console.log ("El color es", color);
+
+let suma = 50 + 50;
+// JAVASCRIPT no hace comprobación de tipos (NO EXISTEN LOS TIPOS DE DATOS)
+
+
+//**************************TYPESCRIPT**************************
+
+// let suma:Number = 10;
+// let ciudad:String = "Almería";
+
+//Angular, Vue, React, Next js.... NO SON LENGUAJES DE PROGRAMACIÓN
+//Son FW de desarrollo y usan JS y TS como lenguaje
